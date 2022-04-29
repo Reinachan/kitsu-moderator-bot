21a6f0a90e62de5cff3dc39d638b250f35f83d0c
Reinachan <ninalouise@reinina.moe>
2022-04-30 01:40:12 +0200

    Updated startup script to work with systemd

--- start.sh
+++ start.sh
@@ -1,24 +1,17 @@
 #!/bin/bash
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-npm start "Startup" "Starting script (this means that the host machine might've been restarted)"
+echo $SCRIPT_DIR
+
+cd $SCRIPT_DIR
+
+git pull
+npm install
+npm start
 
 n=0
 until [ "$n" -ge 5 ]
 do
-  o=$((4-n))
   npm start
-  n=$((n+1))
-  if [ $n -eq 5 ]
-  then
-    sleep 1
-  elif [ $n -eq 4 ]
-  then
-    npm start "Error" "Increasing wait time before final attempt."
-    sleep 120
-  else
-    npm start "Error" "Self-destructing in ${o}"
-    sleep 20
-  fi
+  sleep 20
 done
-
-npm start "Error" "Self-destruction sequence activated. Blowing up."
