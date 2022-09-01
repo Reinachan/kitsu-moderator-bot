53746133698e512ed40be791738d3250fc2b71c0
Nina Louise <ninalouise@reinina.moe>
2022-09-01 21:50:51 +0200

    Make start.sh executable

---
+++ configure-service.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+# set execution permission for start.sh
+sudo chmod +x ./start.sh
+
+# set permissions so only owner has read/write
+sudo chmod 640 ./kitsu-moderator-bot.service
+
+# copy service file to systemd service directory
+sudo cp ./kitsu-moderator-bot.service /etc/systemd/system
+
+# reload the daemon
+sudo systemctl daemon-reload
+
+# enable starting when the computer boots
+sudo systemctl enable kitsu-moderator-bot
+
+# start the service immediately
+sudo systemctl start kitsu-moderator-bot
